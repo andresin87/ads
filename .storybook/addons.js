ccda6c68a61b4f617db0f4cc14d23cb4b3c83f85
Andrés Lucas <alucas@redpoints.com>
2020-01-23 02:24:04 +0100

    Release alpha-1

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -5,5 +5,4 @@ import '@storybook/addon-storysource/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-viewport/register';
-import 'storybook-dark-mode/register';
 import '@storybook/addon-docs/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,10 @@
-import { configure, addDecorator, addParameters } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
-import { themes } from '@storybook/theming';
 
 import '../src/styles/styles.scss';
 
 // global decorators
 addDecorator(withA11y);
-
-// global parameters
-addParameters({
-  darkMode: {
-    // Override the default dark theme
-    dark: { ...themes.dark },
-    // Override the default light theme
-    light: { ...themes.normal },
-  },
-});
 
 // automatically import all files ending in *.stories.(md|js)(x)
 configure(
